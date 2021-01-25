fb32a5a515898e746062e018f79edd3706d01ea8
paters06 <lapc1917@hotmail.com>
2021-01-25 00:45:21 -0500

    Subplots for displacement and stress field components

--- src/postprocessor2D.py
+++ src/postprocessor2D.py
@@ -1,5 +1,6 @@
 # Python libraries
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Local project
 import src.nurbs as rbs
@@ -152,6 +153,88 @@ def stressField(numpoints,U,V,p,q,P,w,dtot,dmat,paramnodes,nodeselem):
 
     return sx,sy,sxy
 
+def plotDisplacementFields(cx,cy,ux,uy):
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+    # Uncomment for cantileverBeam.py
+    # fig, axs = plt.subplots(2,1,sharex='col',sharey='row')
+
+    # Uncomment for pressureCylinder.py and plateWithHole.py
+    fig, axs = plt.subplots(1,2,sharex='col',sharey='row')
+
+    fig.suptitle('Displacement field components')
+    fig.subplots_adjust(hspace=0.4, wspace=0.4)
+
+    field1 = axs[0].pcolormesh(cx,cy,ux,vmin=ux.min(),vmax=ux.max())
+    axs[0].set_title('Ux')
+    axs[0].set_xlabel('x')
+    axs[0].set_ylabel('y')
+    axs[0].set_aspect('equal')
+    divider = make_axes_locatable(axs[0])
+    cax = divider.append_axes("right",size="5%",pad=0.1)
+    cb1 = fig.colorbar(field1,cax=cax,label='[m]')
+
+    field2 = axs[1].pcolormesh(cx,cy,uy,vmin=uy.min(),vmax=uy.max())
+    axs[1].set_title('Uy')
+    axs[1].set_xlabel('x')
+    axs[1].set_ylabel('y')
+    axs[1].set_aspect('equal')
+    divider = make_axes_locatable(axs[1])
+    cax = divider.append_axes("right",size="5%",pad=0.1)
+    cb2 = fig.colorbar(field2,cax=cax,label='[m]')
+
+    plt.show()
+
+def plotStressFields(cx,cy,sx,sy,sxy,svm):
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+    # Uncomment for cantileverBeam.py
+    # fig, axs = plt.subplots(4,1,sharex='col',sharey='row')
+
+    # Uncomment for pressureCylinder.py and plateWithHole.py
+    fig, axs = plt.subplots(2,2,sharex='col',sharey='row')
+
+    fig.suptitle('Stress field components')
+    fig.subplots_adjust(hspace=0.4, wspace=0.4)
+
+    field1 = axs[0,0].pcolormesh(cx,cy,sx,vmin=sx.min(),vmax=sx.max())
+    axs[0,0].set_title('Sx')
+    axs[0,0].set_xlabel('x')
+    axs[0,0].set_ylabel('y')
+    axs[0,0].set_aspect('equal')
+    divider = make_axes_locatable(axs[0,0])
+    cax1 = divider.append_axes("right",size="5%",pad=0.1)
+    cb1 = fig.colorbar(field1,cax=cax1,label='[Pa]')
+
+    field2 = axs[0,1].pcolormesh(cx,cy,sy,vmin=sy.min(),vmax=sy.max())
+    axs[0,1].set_title('Sy')
+    axs[0,1].set_xlabel('x')
+    axs[0,1].set_ylabel('y')
+    axs[0,1].set_aspect('equal')
+    divider = make_axes_locatable(axs[0,1])
+    cax2 = divider.append_axes("right",size="5%",pad=0.1)
+    cb2 = fig.colorbar(field2,cax=cax2,label='[Pa]')
+
+    field3 = axs[1,0].pcolormesh(cx,cy,sxy,vmin=sxy.min(),vmax=sxy.max())
+    axs[1,0].set_title('Sxy')
+    axs[1,0].set_xlabel('x')
+    axs[1,0].set_ylabel('y')
+    axs[1,0].set_aspect('equal')
+    divider = make_axes_locatable(axs[1,0])
+    cax3 = divider.append_axes("right",size="5%",pad=0.1)
+    cb3 = fig.colorbar(field3,cax=cax3,label='[Pa]')
+
+    field4 = axs[1,1].pcolormesh(cx,cy,svm,vmin=svm.min(),vmax=svm.max())
+    axs[1,1].set_title('Von Mises stress')
+    axs[1,1].set_xlabel('x')
+    axs[1,1].set_ylabel('y')
+    axs[1,1].set_aspect('equal')
+    divider = make_axes_locatable(axs[1,1])
+    cax4 = divider.append_axes("right",size="5%",pad=0.1)
+    cb4 = fig.colorbar(field4,cax=cax4,label='[Pa]')
+
+    plt.show()
+
 def postProcessing(U,V,p,q,P,D,w,paramnodes,nodeselem,dtot,dmat):
     numelems = nodeselem.shape[0]
 
@@ -166,5 +249,8 @@ def postProcessing(U,V,p,q,P,D,w,paramnodes,nodeselem,dtot,dmat):
 
     ux,uy,cx,cy = displacementField(numpoints,U,V,p,q,P,D,w,paramnodes,nodeselem)
     sx,sy,sxy = stressField(numpoints,U,V,p,q,P,w,dtot,dmat,paramnodes,nodeselem)
+    svm = np.sqrt( sx**2 - 2*sx*sy + sy**2 + 3*sxy**2 )
     # plts.plotting2DField(cx,cy,ux,P,["Ux Displacement Field","[m]"])
-    plts.plotting2DField(cx,cy,sx,P,["Sx Stress Field","[Pa]"])
+    # plts.plotting2DField(cx,cy,sx,P,["Sx Stress Field","[Pa]"])
+    # plotDisplacementFields(cx,cy,ux,uy)
+    plotStressFields(cx,cy,sx,sy,sxy,svm)
