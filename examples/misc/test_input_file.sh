35c6a53f566395d435e7e163a3ca0dc0f4f686ac
paters06 <paters15@gmail.com>
2025-10-12 15:01:09 -0500

    Cmake implementation

--- examples/misc/test_input_file.sh
+++ examples/misc/test_input_file.sh
@@ -2,7 +2,7 @@ tput reset
 gfortran -c -g ../../src/spline_functions/utils.f90
 
 gfortran -c -g -fbounds-check read_input_file.f90 utils.o
-gfortran -L/usr/lib/x86_64-linux-gnu/ -o read_input_file read_input_file.o utils.o
+gfortran -o read_input_file read_input_file.o utils.o
 rm *.o
 rm *.mod
 # cd ../../examples/geometry/
